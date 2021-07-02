fe10b08c4b7b2071921be8cb1a89517880d557b9
CyrilPeponnet <cyril@peponnet.fr>
2021-07-01 17:33:30 -0700

    Update version

--- internal/configuration/versions.go
+++ internal/configuration/versions.go
@@ -3,7 +3,7 @@ package configuration
 // Various version information.
 var (
 	ProjectName    = "tracer"
-	ProjectVersion = "v1.1.0"
-	ProjectSha     = "599ee9413cce3dcacf53fe18a3916e885c291290"
+	ProjectVersion = "v1.3.0"
+	ProjectSha     = "5385d4877090a4a615ec30ecf004d5d3e1696449"
 	ProjectRelease = "dev"
 )
